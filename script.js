8c7c25f715ad63d30d251d7ed8f17ebc3c635cc4
Carolina Serrano <adrianaserrano185@outlook.com>
2023-07-23 08:47:37 -0700

    Declare variables for the operations buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+let display=document.getElementById('display');
+let exp=document.getElementById('exp');
+let root=document.getElementById('root');
+let del=document.getElementById('del');
+let ac=document.getElementById('ac');
+let division=document.getElementById('division');
+let rest=document.getElementById('rest');
+let dot=document.getElementById('dot');
+let equal=document.getElementById('equal');
+let add=document.getElementById('add');
+let multiply=document.getElementById('multiply');
